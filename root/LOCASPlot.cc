4b80c17361e5fdbdf5d1c295863d27f50d5cc81a
Robert Stainforth (PhD 2012) <rpfs@hep158.ph.liv.ac.uk>
2014-01-08 15:42:00 +0000

    Updated LOCASPlot.cc with more refractive media correlation functions

--- root/LOCASPlot.cc
+++ root/LOCASPlot.cc
@@ -1,6 +1,7 @@
 #include "RAT/DS/Root.hh"
 #include "RAT/DS/EV.hh"
 #include "RAT/DS/Run.hh"
+#include "RAT/DS/LightPath.hh"
 
 #include "TObject.h"
 #include "TH1D.h"
@@ -10,6 +11,7 @@
 #include "TFile.h"
 #include "TVector3.h"
 #include "TCanvas.h"
+#include "TLegend.h"
 
 #include "LOCASLightPath.hh"
 
@@ -34,7 +36,7 @@ LoadRootFile( const char* lpFile,
 
 
 void
-LOCASDscVDav( const char* root_file, 
+LOCASDavVDsc( const char* root_file, 
 	      const char* plot_file_name,
 	      const Double_t innerR,
 	      const Double_t outerR )
@@ -76,9 +78,9 @@ LOCASDscVDav( const char* root_file,
       // Loop over the PMTCals i.e. the PMTs registering a hit
       for( Int_t iPMT = 0; iPMT < rEV->GetPMTCalCount(); iPMT++ ){
 
-      // Ensure start position is less than "innerR" and 
-      // greater than "outerR"
-	if( startPos.Mag() < outerR && startPos.Mag() > innerR ){
+        // Ensure start position is less than "innerR" and 
+        // greater than "outerR"
+	if( ( startPos.Mag() < outerR ) && ( startPos.Mag() > innerR ) ){
 	  
 	  // Get the PMT ID
 	  Int_t pmtID = rEV->GetPMTCal( iPMT )->GetID();
@@ -87,31 +89,35 @@ LOCASDscVDav( const char* root_file,
 	  TVector3 pmtPos = rPMTProp->GetPos( pmtID );
 
 	  lRLP.CalculatePath( startPos, pmtPos, 10.0, 400.0 );
-	  lSLP.CalculatePath( startPos, pmtPos, 10.0, 400.0 );
+	  lSLP.CalculatePath( startPos, pmtPos );
 
-	  if ( !lRLP.GetTIR() && !lRLP.GetResvHit() && !lRLP.GetXAVNeck() ){
-
+	  if ( !lRLP.GetTIR() && !lRLP.GetResvHit() ){
 	    RLPGraph->SetPoint( RLPPoint++, lRLP.GetDistInScint()/10.0, lRLP.GetDistInAV()/10.0 );
 	    SLPGraph->SetPoint( SLPPoint++, lSLP.GetDistInScint()/10.0, lSLP.GetDistInAV()/10.0 );
-
 	  }
-
 	  lRLP.Clear();
 	  lSLP.Clear();
+
 	}
       }
     }
   }
 
-  TCanvas* c1 = new TCanvas( "c1", "Scintillator vs. AV Distance", 640, 400 );
+  TCanvas* c1 = new TCanvas( "c-LOCAS-DavVDsc", "LOCAS: AV vs. Scintillator Distance", 640, 400 );
 
-  RLPGraph->SetTitle( "Scintillator vs. AV Distance" );
-  RLPGraph->GetXaxis()->SetTitle( "AV Distance [cm]" );
-  RLPGraph->GetYaxis()->SetTitle( "Water Distance [cm]" );
-  RLPGraph->GetYaxis()->SetTitleOffset( 1.15 );
+  //RLPGraph->SetTitle( "LOCAS: AV vs. Scintillator Distance" );
+  RLPGraph->GetXaxis()->SetTitle( "Scintillator Distance [cm]" );
+  RLPGraph->GetYaxis()->SetTitle( "AV Distance [cm]" );
+  //RLPGraph->GetYaxis()->SetTitleOffset( 1.0 );
 
   RLPGraph->SetMarkerColor( 1 );
+  RLPGraph->SetLineColor( 1 );
+
   SLPGraph->SetMarkerColor( 2 );
+  SLPGraph->SetLineColor( 2 );
+
+  RLPGraph->SetLineWidth( 2 );
+  SLPGraph->SetLineWidth( 2 );
 
   RLPGraph->SetMarkerStyle( 1 );
   SLPGraph->SetMarkerStyle( 1 );
@@ -119,9 +125,546 @@ LOCASDscVDav( const char* root_file,
   RLPGraph->Draw( "AP" );
   SLPGraph->Draw( "P, same" );
 
-  c1->Print(plot_file_name);	
+  TLegend* myLeg = new TLegend( 0.65, 0.65, 0.85, 0.85 );
+
+  myLeg->AddEntry( RLPGraph, "Refracted Path", "l" );
+  myLeg->AddEntry( SLPGraph, "Straight Path", "l" );
+
+  myLeg->Draw( "same" );
+  c1->Print(plot_file_name);
+
+  return;
 
 }
+
+void
+DavVDsc( const char* root_file, 
+	 const char* plot_file_name,
+	 const Double_t innerR,
+	 const Double_t outerR )
+{
+  
+  // Load the ROOT File first
+  RAT::DS::Root* rDS;
+  RAT::DS::Run* rRun;
+  TTree* tree;
+  LoadRootFile( root_file, &tree, &rDS, &rRun );
+  
+  // The graphs which will hold the refracted and straight
+  // light path information respectively.
+  TGraph* RLPGraph = new TGraph();
+  TGraph* SLPGraph = new TGraph();
+
+  Int_t RLPPoint = 0;
+  Int_t SLPPoint = 0;
+
+  for( Int_t iEvent = 0; iEvent < tree->GetEntries(); iEvent++ ){
+
+    tree->GetEntry( iEvent );
+
+    RAT::DS::MC* rMC = rDS->GetMC();
+    RAT::DS::MCParticle* rMCParticle = rMC->GetMCParticle( 0 );
+
+    // "startPos" is the true event position as determined by the MC
+    TVector3 startPos = rMCParticle->GetPos();
+
+    // Loop over the triggered events
+    for( Int_t iEV = 0; iEV < rDS->GetEVCount(); iEV++ ){
+
+      RAT::DS::EV* rEV = rDS->GetEV( iEV );
+      RAT::DS::PMTProperties* rPMTProp = rRun->GetPMTProp();
+
+      // Loop over the PMTCals i.e. the PMTs registering a hit
+      for( Int_t iPMT = 0; iPMT < rEV->GetPMTCalCount(); iPMT++ ){
+
+        // Ensure start position is less than "innerR" and 
+        // greater than "outerR"
+	if( ( startPos.Mag() < outerR ) && ( startPos.Mag() > innerR ) ){
+
+	  RAT::DS::LightPath* lRLPath = rRun->GetLightPath();
+	  RAT::DS::LightPath* lSLPath = rRun->GetLightPath();
+
+	  Double_t distInScint, distInAV, distInWater;
+	  Double_t distInScint1, distInAV1, distInWater1;
+	  TVector3 hypPMTPos;
+	  
+	  // Get the PMT ID
+	  Int_t pmtID = rEV->GetPMTCal( iPMT )->GetID();
+	  
+	  // Get the PMT Position
+	  TVector3 pmtPos = rPMTProp->GetPos( pmtID );
+
+	  lRLPath->CalcByPosition( startPos, pmtPos, hypPMTPos, distInScint, distInAV, distInWater, 400.0, 10.0 );
+	  lSLPath->CalcByPosition( startPos, pmtPos, distInScint1, distInAV1, distInWater1 );
+
+	  if ( !lRLPath->GetTIR() && !lRLPath->GetResvHit() ){
+	    RLPGraph->SetPoint( RLPPoint++, distInScint/10.0, distInAV/10.0 );
+	    SLPGraph->SetPoint( SLPPoint++, distInScint1/10.0, distInAV1/10.0 );
+	  }
+	}
+      }
+    }
+  }
+
+  TCanvas* c1 = new TCanvas( "c-DavVDsc", "RAT: AV vs. Scintillator Distance", 640, 400 );
+
+  //RLPGraph->SetTitle( "RAT AV vs. Scintillator Distance" );
+  RLPGraph->GetXaxis()->SetTitle( "Scintillator Distance [cm]" );
+  RLPGraph->GetYaxis()->SetTitle( "AV Distance [cm]" );
+  //RLPGraph->GetYaxis()->SetTitleOffset( 1.10 );
+
+  RLPGraph->SetMarkerColor( 1 );
+  RLPGraph->SetLineColor( 1 );
+
+  SLPGraph->SetMarkerColor( 2 );
+  SLPGraph->SetLineColor( 2 );
+
+  RLPGraph->SetLineWidth( 2 );
+  SLPGraph->SetLineWidth( 2 );
+
+  RLPGraph->SetMarkerStyle( 1 );
+  SLPGraph->SetMarkerStyle( 1 );
+
+  RLPGraph->Draw( "AP" );
+  SLPGraph->Draw( "P, same" );
+
+  TLegend* myLeg = new TLegend( 0.65, 0.65, 0.85, 0.85 );
+
+  myLeg->AddEntry( RLPGraph, "Refracted Path", "l" );
+  myLeg->AddEntry( SLPGraph, "Straight Path", "l" );
+
+  myLeg->Draw( "same" );
+
+  c1->Print(plot_file_name);
+
+  return;
+
+}
+
+void
+LOCASDh2oVDsc( const char* root_file, 
+	      const char* plot_file_name,
+	      const Double_t innerR,
+	      const Double_t outerR )
+{
+
+  // Load the ROOT File first
+  RAT::DS::Root* rDS;
+  RAT::DS::Run* rRun;
+  TTree* tree;
+  LoadRootFile( root_file, &tree, &rDS, &rRun );
+
+  // The graphs which will hold the refracted and straight
+  // light path information respectively.
+  TGraph* RLPGraph = new TGraph();
+  TGraph* SLPGraph = new TGraph();
+
+  LOCAS::LOCASLightPath lRLP;
+  LOCAS::LOCASLightPath lSLP;
+
+  Int_t RLPPoint = 0;
+  Int_t SLPPoint = 0;
+
+  for( Int_t iEvent = 0; iEvent < tree->GetEntries(); iEvent++ ){
+
+    tree->GetEntry( iEvent );
+
+    RAT::DS::MC* rMC = rDS->GetMC();
+    RAT::DS::MCParticle* rMCParticle = rMC->GetMCParticle( 0 );
+
+    // "startPos" is the true event position as determined by the MC
+    TVector3 startPos = rMCParticle->GetPos();
+
+    // Loop over the triggered events
+    for( Int_t iEV = 0; iEV < rDS->GetEVCount(); iEV++ ){
+
+      RAT::DS::EV* rEV = rDS->GetEV( iEV );
+      RAT::DS::PMTProperties* rPMTProp = rRun->GetPMTProp();
+
+      // Loop over the PMTCals i.e. the PMTs registering a hit
+      for( Int_t iPMT = 0; iPMT < rEV->GetPMTCalCount(); iPMT++ ){
+
+        // Ensure start position is less than "innerR" and 
+        // greater than "outerR"
+	if( ( startPos.Mag() < outerR ) && ( startPos.Mag() > innerR ) ){
+	  
+	  // Get the PMT ID
+	  Int_t pmtID = rEV->GetPMTCal( iPMT )->GetID();
+	  
+	  // Get the PMT Position
+	  TVector3 pmtPos = rPMTProp->GetPos( pmtID );
+
+	  lRLP.CalculatePath( startPos, pmtPos, 10.0, 400.0 );
+	  lSLP.CalculatePath( startPos, pmtPos );
+
+	  if ( !lRLP.GetTIR() && !lRLP.GetResvHit() ){
+	    RLPGraph->SetPoint( RLPPoint++, lRLP.GetDistInScint()/10.0, lRLP.GetDistInWater()/10.0 );
+	    SLPGraph->SetPoint( SLPPoint++, lSLP.GetDistInScint()/10.0, lSLP.GetDistInWater()/10.0 );
+	  }
+	  lRLP.Clear();
+	  lSLP.Clear();
+
+	}
+      }
+    }
+  }
+
+  TCanvas* c1 = new TCanvas( "c-LOCASDh2oVDsc", "LOCAS: Water vs. Scintillator Distance", 640, 400 );
+
+  //RLPGraph->SetTitle( "LOCAS: Water vs. Scintillator Distance" );
+  RLPGraph->GetXaxis()->SetTitle( "Scintillator Distance [cm]" );
+  RLPGraph->GetYaxis()->SetTitle( "Water Distance [cm]" );
+  //RLPGraph->GetYaxis()->SetTitleOffset( 1.10 );
+
+  RLPGraph->SetMarkerColor( 1 );
+  RLPGraph->SetLineColor( 1 );
+
+  SLPGraph->SetMarkerColor( 2 );
+  SLPGraph->SetLineColor( 2 );
+
+  RLPGraph->SetLineWidth( 2 );
+  SLPGraph->SetLineWidth( 2 );
+
+  RLPGraph->SetMarkerStyle( 1 );
+  SLPGraph->SetMarkerStyle( 1 );
+
+  RLPGraph->Draw( "AP" );
+  SLPGraph->Draw( "P, same" );
+
+  TLegend* myLeg = new TLegend( 0.65, 0.65, 0.85, 0.85 );
+
+  myLeg->AddEntry( RLPGraph, "Refracted Path", "l" );
+  myLeg->AddEntry( SLPGraph, "Straight Path", "l" );
+
+  myLeg->Draw( "same" );
+  c1->Print(plot_file_name);
+
+  return;
+
+}
+
+void
+Dh2oVDsc( const char* root_file, 
+	 const char* plot_file_name,
+	 const Double_t innerR,
+	 const Double_t outerR )
+{
+  
+  // Load the ROOT File first
+  RAT::DS::Root* rDS;
+  RAT::DS::Run* rRun;
+  TTree* tree;
+  LoadRootFile( root_file, &tree, &rDS, &rRun );
+  
+  // The graphs which will hold the refracted and straight
+  // light path information respectively.
+  TGraph* RLPGraph = new TGraph();
+  TGraph* SLPGraph = new TGraph();
+
+  Int_t RLPPoint = 0;
+  Int_t SLPPoint = 0;
+
+  for( Int_t iEvent = 0; iEvent < tree->GetEntries(); iEvent++ ){
+
+    tree->GetEntry( iEvent );
+
+    RAT::DS::MC* rMC = rDS->GetMC();
+    RAT::DS::MCParticle* rMCParticle = rMC->GetMCParticle( 0 );
+
+    // "startPos" is the true event position as determined by the MC
+    TVector3 startPos = rMCParticle->GetPos();
+
+    // Loop over the triggered events
+    for( Int_t iEV = 0; iEV < rDS->GetEVCount(); iEV++ ){
+
+      RAT::DS::EV* rEV = rDS->GetEV( iEV );
+      RAT::DS::PMTProperties* rPMTProp = rRun->GetPMTProp();
+
+      // Loop over the PMTCals i.e. the PMTs registering a hit
+      for( Int_t iPMT = 0; iPMT < rEV->GetPMTCalCount(); iPMT++ ){
+
+        // Ensure start position is less than "innerR" and 
+        // greater than "outerR"
+	if( ( startPos.Mag() < outerR ) && ( startPos.Mag() > innerR ) ){
+
+	  RAT::DS::LightPath* lRLPath = rRun->GetLightPath();
+	  RAT::DS::LightPath* lSLPath = rRun->GetLightPath();
+
+	  Double_t distInScint, distInAV, distInWater;
+	  Double_t distInScint1, distInAV1, distInWater1;
+	  TVector3 hypPMTPos;
+	  
+	  // Get the PMT ID
+	  Int_t pmtID = rEV->GetPMTCal( iPMT )->GetID();
+	  
+	  // Get the PMT Position
+	  TVector3 pmtPos = rPMTProp->GetPos( pmtID );
+
+	  lRLPath->CalcByPosition( startPos, pmtPos, hypPMTPos, distInScint, distInAV, distInWater, 400.0, 10.0 );
+	  lSLPath->CalcByPosition( startPos, pmtPos, distInScint1, distInAV1, distInWater1 );
+
+	  if ( !lRLPath->GetTIR() && !lRLPath->GetResvHit() ){
+	    RLPGraph->SetPoint( RLPPoint++, distInScint/10.0, distInWater/10.0 );
+	    SLPGraph->SetPoint( SLPPoint++, distInScint1/10.0, distInWater1/10.0 );
+	  }
+	}
+      }
+    }
+  }
+
+  TCanvas* c1 = new TCanvas( "c-Dh2oVDsc", "RAT: Water vs. Scintillator Distance", 640, 400 );
+
+  //RLPGraph->SetTitle( "RAT Water vs. Scintillator Distance" );
+  RLPGraph->GetXaxis()->SetTitle( "Scintillator Distance [cm]" );
+  RLPGraph->GetYaxis()->SetTitle( "Water Distance [cm]" );
+  //RLPGraph->GetYaxis()->SetTitleOffset( 1.10 );
+
+  RLPGraph->SetMarkerColor( 1 );
+  RLPGraph->SetLineColor( 1 );
+
+  SLPGraph->SetMarkerColor( 2 );
+  SLPGraph->SetLineColor( 2 );
+
+  RLPGraph->SetLineWidth( 2 );
+  SLPGraph->SetLineWidth( 2 );
+
+  RLPGraph->SetMarkerStyle( 1 );
+  SLPGraph->SetMarkerStyle( 1 );
+
+  RLPGraph->Draw( "AP" );
+  SLPGraph->Draw( "P, same" );
+
+  TLegend* myLeg = new TLegend( 0.65, 0.65, 0.85, 0.85 );
+
+  myLeg->AddEntry( RLPGraph, "Refracted Path", "l" );
+  myLeg->AddEntry( SLPGraph, "Straight Path", "l" );
+
+  myLeg->Draw( "same" );
+
+  c1->Print(plot_file_name);
+
+  return;
+
+}
+
+void
+LOCASDavVDh2o( const char* root_file, 
+	       const char* plot_file_name,
+	       const Double_t innerR,
+	       const Double_t outerR )
+{
+
+  // Load the ROOT File first
+  RAT::DS::Root* rDS;
+  RAT::DS::Run* rRun;
+  TTree* tree;
+  LoadRootFile( root_file, &tree, &rDS, &rRun );
+
+  // The graphs which will hold the refracted and straight
+  // light path information respectively.
+  TGraph* RLPGraph = new TGraph();
+  TGraph* SLPGraph = new TGraph();
+
+  LOCAS::LOCASLightPath lRLP;
+  LOCAS::LOCASLightPath lSLP;
+
+  Int_t RLPPoint = 0;
+  Int_t SLPPoint = 0;
+
+  for( Int_t iEvent = 0; iEvent < tree->GetEntries(); iEvent++ ){
+
+    tree->GetEntry( iEvent );
+
+    RAT::DS::MC* rMC = rDS->GetMC();
+    RAT::DS::MCParticle* rMCParticle = rMC->GetMCParticle( 0 );
+
+    // "startPos" is the true event position as determined by the MC
+    TVector3 startPos = rMCParticle->GetPos();
+
+    // Loop over the triggered events
+    for( Int_t iEV = 0; iEV < rDS->GetEVCount(); iEV++ ){
+
+      RAT::DS::EV* rEV = rDS->GetEV( iEV );
+      RAT::DS::PMTProperties* rPMTProp = rRun->GetPMTProp();
+
+      // Loop over the PMTCals i.e. the PMTs registering a hit
+      for( Int_t iPMT = 0; iPMT < rEV->GetPMTCalCount(); iPMT++ ){
+
+        // Ensure start position is less than "innerR" and 
+        // greater than "outerR"
+	if( ( startPos.Mag() < outerR ) && ( startPos.Mag() > innerR ) ){
+	  
+	  // Get the PMT ID
+	  Int_t pmtID = rEV->GetPMTCal( iPMT )->GetID();
+	  
+	  // Get the PMT Position
+	  TVector3 pmtPos = rPMTProp->GetPos( pmtID );
+
+	  lRLP.CalculatePath( startPos, pmtPos, 400.0, 10.0 );
+	  lSLP.CalculatePath( startPos, pmtPos );
+
+	  if ( !lRLP.GetTIR() && !lRLP.GetResvHit() ){
+	    RLPGraph->SetPoint( RLPPoint++, lRLP.GetDistInWater()/10.0, lRLP.GetDistInAV()/10.0 );
+	    SLPGraph->SetPoint( SLPPoint++, lSLP.GetDistInWater()/10.0, lSLP.GetDistInAV()/10.0 );
+	  }
+	  lRLP.Clear();
+	  lSLP.Clear();
+
+	}
+      }
+    }
+  }
+
+  TCanvas* c1 = new TCanvas( "c-LOCASDavVDh2o", "LOCAS: AV vs. Water Distance", 640, 400 );
+
+  //RLPGraph->SetTitle( "LOCAS: AV vs. Water Distance" );
+  RLPGraph->GetXaxis()->SetTitle( "Water Distance [cm]" );
+  RLPGraph->GetYaxis()->SetTitle( "AV Distance [cm]" );
+  //RLPGraph->GetYaxis()->SetTitleOffset( 1.10 );
+
+  RLPGraph->SetMarkerColor( 1 );
+  RLPGraph->SetLineColor( 1 );
+
+  SLPGraph->SetMarkerColor( 2 );
+  SLPGraph->SetLineColor( 2 );
+
+  RLPGraph->SetLineWidth( 2 );
+  SLPGraph->SetLineWidth( 2 );
+
+  RLPGraph->SetMarkerStyle( 1 );
+  SLPGraph->SetMarkerStyle( 1 );
+
+  RLPGraph->Draw( "AP" );
+  SLPGraph->Draw( "P, same" );
+
+  TLegend* myLeg = new TLegend( 0.60, 0.30, 0.80, 0.50 );
+
+  myLeg->AddEntry( RLPGraph, "Refracted Path", "l" );
+  myLeg->AddEntry( SLPGraph, "Straight Path", "l" );
+
+  myLeg->Draw( "same" );
+  c1->Print(plot_file_name);
+
+  return;
+
+}
+
+void
+DavVDh2o( const char* root_file, 
+	 const char* plot_file_name,
+	 const Double_t innerR,
+	 const Double_t outerR )
+{
+  
+  // Load the ROOT File first
+  RAT::DS::Root* rDS;
+  RAT::DS::Run* rRun;
+  TTree* tree;
+  LoadRootFile( root_file, &tree, &rDS, &rRun );
+  
+  // The graphs which will hold the refracted and straight
+  // light path information respectively.
+  TGraph* RLPGraph = new TGraph();
+  TGraph* SLPGraph = new TGraph();
+
+  Int_t RLPPoint = 0;
+  Int_t SLPPoint = 0;
+
+  for( Int_t iEvent = 0; iEvent < tree->GetEntries(); iEvent++ ){
+
+    tree->GetEntry( iEvent );
+
+    RAT::DS::MC* rMC = rDS->GetMC();
+    RAT::DS::MCParticle* rMCParticle = rMC->GetMCParticle( 0 );
+
+    // "startPos" is the true event position as determined by the MC
+    TVector3 startPos = rMCParticle->GetPos();
+
+    // Loop over the triggered events
+    for( Int_t iEV = 0; iEV < rDS->GetEVCount(); iEV++ ){
+
+      RAT::DS::EV* rEV = rDS->GetEV( iEV );
+      RAT::DS::PMTProperties* rPMTProp = rRun->GetPMTProp();
+
+      // Loop over the PMTCals i.e. the PMTs registering a hit
+      for( Int_t iPMT = 0; iPMT < rEV->GetPMTCalCount(); iPMT++ ){
+
+        // Ensure start position is less than "innerR" and 
+        // greater than "outerR"
+	if( ( startPos.Mag() < outerR ) && ( startPos.Mag() > innerR ) ){
+
+	  RAT::DS::LightPath* lRLPath = rRun->GetLightPath();
+	  RAT::DS::LightPath* lSLPath = rRun->GetLightPath();
+
+	  Double_t distInScint, distInAV, distInWater;
+	  Double_t distInScint1, distInAV1, distInWater1;
+	  TVector3 hypPMTPos;
+	  
+	  // Get the PMT ID
+	  Int_t pmtID = rEV->GetPMTCal( iPMT )->GetID();
+	  
+	  // Get the PMT Position
+	  TVector3 pmtPos = rPMTProp->GetPos( pmtID );
+
+	  lRLPath->CalcByPosition( startPos, pmtPos, hypPMTPos, distInScint, distInAV, distInWater, 400.0, 10.0 );
+	  lSLPath->CalcByPosition( startPos, pmtPos, distInScint1, distInAV1, distInWater1 );
+
+	  if ( !lRLPath->GetTIR() && !lRLPath->GetResvHit() ){
+	    RLPGraph->SetPoint( RLPPoint++, distInWater/10.0, distInAV/10.0 );
+	    SLPGraph->SetPoint( SLPPoint++, distInWater1/10.0, distInAV1/10.0 );
+	  }
+	}
+      }
+    }
+  }
+
+  TCanvas* c1 = new TCanvas( "c-DavVDh2o", "RAT: AV vs. Water Distance", 640, 400 );
+
+  //RLPGraph->SetTitle( "RAT: AV vs. Water Distance" );
+  RLPGraph->GetXaxis()->SetTitle( "Water Distance [cm]" );
+  RLPGraph->GetYaxis()->SetTitle( "AV Distance [cm]" );
+  //RLPGraph->GetYaxis()->SetTitleOffset( 1.10 );
+
+  RLPGraph->SetMarkerColor( 1 );
+  RLPGraph->SetLineColor( 1 );
+
+  SLPGraph->SetMarkerColor( 2 );
+  SLPGraph->SetLineColor( 2 );
+
+  RLPGraph->SetLineWidth( 2 );
+  SLPGraph->SetLineWidth( 2 );
+
+  RLPGraph->SetMarkerStyle( 1 );
+  SLPGraph->SetMarkerStyle( 1 );
+
+  RLPGraph->Draw( "AP" );
+  SLPGraph->Draw( "P, same" );
+
+  TLegend* myLeg = new TLegend( 0.60, 0.30, 0.80, 0.50 );
+
+  myLeg->AddEntry( RLPGraph, "Refracted Path", "l" );
+  myLeg->AddEntry( SLPGraph, "Straight Path", "l" );
+
+  myLeg->Draw( "same" );
+
+  c1->Print(plot_file_name);
+
+  return;
+
+}
+
+void
+PlotAll( const char* root_file )
+{
+
+  LOCASDavVDh2o( root_file, "locas_h2o_vs_av.eps", 0.0, 6005.3 );
+  LOCASDavVDsc( root_file, "locas_av_vs_sc.eps", 0.0, 6005.3 );
+  LOCASDh2oVDsc( root_file, "locas_h2o_vs_sc.eps", 0.0, 6005.3 );
+  DavVDh2o( root_file, "rat_h2o_vs_av.eps", 0.0, 6005.3 );
+  DavVDsc( root_file, "rat_av_vs_sc.eps", 0.0, 6005.3 );
+  Dh2oVDsc( root_file, "rat_h2o_vs_sc.eps", 0.0, 6005.3 );
+
+} 
 
 
 
