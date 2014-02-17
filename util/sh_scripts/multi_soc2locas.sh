b47e34cca09a360e28c72daea4df5ea97924a0ba
RStainforth <rob@rpfs-mac.local>
2014-02-17 10:20:51 +0000

    Added sh_scripts directory in the util folder to allow for multiple db2soc and soc2locas runs to be made

---
+++ util/sh_scripts/multi_soc2locas.sh
@@ -0,0 +1,18 @@
+source ${LOCAS_ROOT}/env_locas.sh
+
+soc2locas -r 12300 -c 12300 -w 12300
+
+soc2locas -r 11200 -c 12300 -w 12300
+soc2locas -r 12200 -c 12300 -w 12300
+soc2locas -r 11400 -c 12300 -w 12300
+soc2locas -r 12400 -c 12300 -w 12300
+
+soc2locas -r 21200 -c 12300 -w 12300
+soc2locas -r 22200 -c 12300 -w 12300
+soc2locas -r 21400 -c 12300 -w 12300
+soc2locas -r 22400 -c 12300 -w 12300
+
+soc2locas -r 31200 -c 12300 -w 12300
+soc2locas -r 32200 -c 12300 -w 12300
+soc2locas -r 31400 -c 12300 -w 12300
+soc2locas -r 32400 -c 12300 -w 12300
