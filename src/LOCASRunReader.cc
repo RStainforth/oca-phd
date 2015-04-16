275fb33d1519ca7c956f5adde3a8d1ad2d8ad9e2
RStainforth <rob@MacBook-Pro.local>
2015-04-16 16:43:55 +0100

    Cleaned up LOCASRunReader commenting and functionality

--- src/LOCASRunReader.cc
+++ src/LOCASRunReader.cc
@@ -1,31 +1,14 @@
-///////////////////////////////////////////////////////////////////
-///
-/// FILENAME: LOCASRunReader.cc
-///
-/// CLASS: LOCAS::LOCASRunReader
-///
-/// BRIEF: Reader class for the LOCASRun Objects
-///          
-/// AUTHOR: Rob Stainforth [RPFS] <rpfs@liv.ac.uk>
-///
-/// REVISION HISTORY:\n
-///     02/2014 : RPFS - First Revision, new file. \n
-///
-////////////////////////////////////////////////////////////////////
+#include "LOCASRun.hh"
+#include "LOCASDB.hh"
+#include "LOCASRunReader.hh"
 
-#include <TFile.h>
-#include <TObject.h>
+#include "TFile.h"
+#include "TChain.h"
 
 #include <iostream>
 #include <sstream>
 #include <string>
 
-#include "LOCASRun.hh"
-#include "LOCASDB.hh"
-#include "LOCASRunReader.hh"
-
-#include "TMath.h"
-#include "TMinuit.h"
 
 using namespace LOCAS;
 using namespace std;
@@ -36,54 +19,87 @@ using namespace std;
 LOCASRunReader::LOCASRunReader()
 {
 
+  // Create a new TChain object with the name
+  // "LOCASRunT".
   fLOCASRunT = new TChain( "LOCASRunT" );
   
+  // Create a new LOCASRun object and set the branch
+  // address to the memory allocated to it on the
+  // TChain.
   fLOCASRun = new LOCASRun();
   fLOCASRunT->SetBranchAddress( "LOCASRun", &fLOCASRun );
 
+  // Set the next counter to zero to begin with.
   fNext = 0;
-  fNLOCASRuns = fLOCASRunT->GetEntries();
+
+  // Set the number of LOCASRun object to zero to begin with.
+  // None have been added as part of the constructor.
+  fNLOCASRuns = 0;
 
 }
 
 //////////////////////////////////////
 //////////////////////////////////////
 
-LOCASRunReader::LOCASRunReader( std::vector< Int_t >& runIDs )
+LOCASRunReader::LOCASRunReader( vector< Int_t >& runIDs )
 {
 
+  // Create a stringstream object and string to help
+  // create the file path for the various LOCASRun files
+  // associated with each run ID in the 'runIDs' vector.
   stringstream myStream;
   string runIDStr = "";
 
+  // Create an instance of the LOCASDB object to 
+  // obtain the full system path directory to 
+  // the LOCASRun folder.
   LOCASDB lDB;
   string locasRunDir = lDB.GetLOCASRunDir();
   string fExt = "_LOCASRun.root";
   
   string filename = "";
 
+  // Create a new TChain to store all the different
+  // LOCASRun .root files.
   fLOCASRunT = new TChain( "LOCASRunT" );
 
+  // Create a new LOCASRun object.
   fLOCASRun = new LOCASRun();
   fLOCASRunT->SetBranchAddress( "LOCASRun", &fLOCASRun );
 
+  // For each run ID in the 'runIDs' vector find the associated
+  // LOCASRun file and add it to the reader.
   for ( Int_t iRun = 0; iRun < (Int_t)runIDs.size(); iRun++ ){
 
     myStream << runIDs[ iRun ];
     myStream >> runIDStr;
+
+    // The full system path to the LOCASRun file.
     filename = ( locasRunDir + runIDStr + fExt );
+
+    // Add the file.
     Add( filename.c_str() );
 
+    // Clear the stringstream and string variables
+    // in preparation for the next iteration of the loop.
     myStream.clear();
     runIDStr = "";
 
   }
 
-  
+  // Set the next counter to zero to begin with.
   fNext = 0;
+
+  // Set the number of LOCASRuns to the number which have
+  // been added to the TChain.
   fNLOCASRuns = fLOCASRunT->GetEntries();
 
+  // Set the pointer to the current LOCASRun object to the
+  // first entry in the TChain.
   fLOCASRun = GetLOCASRun( runIDs[ 0 ] );
-  std::map< Int_t, LOCASPMT >::iterator iPMT = fLOCASRun->GetLOCASPMTIterBegin();
+
+  // Set the current PMT pointer to the first PMT in the first LOCASRun entry.
+  map< Int_t, LOCASPMT >::iterator iPMT = fLOCASRun->GetLOCASPMTIterBegin();
   fCurrentPMT = &( iPMT->second );
   
 
@@ -94,30 +110,51 @@ LOCASRunReader::LOCASRunReader( std::vector< Int_t >& runIDs )
 
 LOCASRunReader::LOCASRunReader( Int_t runID )
 {
+
+  // Create a LOCASRunReader using just a single LOCASRun file.
+  // This is the same as the above constructor but just for a single
+  // LOCASRun file.
+
+  // The stringstream and string variables for creating
+  // the full system path to the LOCASRun file.
   stringstream myStream;
   myStream << runID;
 
   string runIDStr = "";
   myStream >> runIDStr;
 
+  // Get the full system path to the LOCASRun directory.
   LOCASDB lDB;
   string locasRunDir = lDB.GetLOCASRunDir();
   string fExt = "_LOCASRun.root";
   
+  // Create the full path name.
   string filename = ( locasRunDir + runIDStr + fExt );  
 
+  // Create a TChain object to store the LOCASRun .root file on.
   fLOCASRunT = new TChain( "LOCASRunT" );
 
+  // Create a pointer to the LOCASRun object.
   fLOCASRun = new LOCASRun();
   fLOCASRunT->SetBranchAddress( "LOCASRun", &fLOCASRun );
 
+  // Add the LOCASRun .root file to the LOCASRunReader.
   Add( filename.c_str() );
 
+  // Set the next counter to zero to begin with.
   fNext = 0;
+
+  // Set the number of LOCASRuns currenty on this reader
+  // to the number of entries on the TChain to begin with.
   fNLOCASRuns = fLOCASRunT->GetEntries();
 
+  // Set the pointer to the LOCASRun object.
   fLOCASRun = GetLOCASRun( runID );
-  std::map< Int_t, LOCASPMT >::iterator iPMT = fLOCASRun->GetLOCASPMTIterBegin();
+
+  // Set the pointer to the current PMT to be the first LOCASPMT
+  // object on the first (and only in this case) LOCASRun object
+  // on the TChain.
+  map< Int_t, LOCASPMT >::iterator iPMT = fLOCASRun->GetLOCASPMTIterBegin();
   fCurrentPMT = &( iPMT->second );
 
 }
@@ -128,6 +165,8 @@ LOCASRunReader::LOCASRunReader( Int_t runID )
 LOCASRunReader::LOCASRunReader( const char* filename )
 {
 
+  // Add the LOCASRun file directory just using the full
+  // system path of the LOCASRun file.
   fLOCASRunT = new TChain( "LOCASRunT" );
 
   fLOCASRun = new LOCASRun();
@@ -138,7 +177,7 @@ LOCASRunReader::LOCASRunReader( const char* filename )
 
   Add( filename );
 
-  std::map< Int_t, LOCASPMT >::iterator iPMT = fLOCASRun->GetLOCASPMTIterBegin();
+  map< Int_t, LOCASPMT >::iterator iPMT = fLOCASRun->GetLOCASPMTIterBegin();
   fCurrentPMT = &( iPMT->second );
 
 }
@@ -149,6 +188,8 @@ LOCASRunReader::LOCASRunReader( const char* filename )
 LOCASRunReader::~LOCASRunReader()
 {
 
+  // Delete the memory associated with the TChain 'fLOCASRunT'
+  // and the LOCASRun object pointer 'fLOCASRun'.
   delete fLOCASRunT;
   delete fLOCASRun;
 
@@ -160,6 +201,7 @@ LOCASRunReader::~LOCASRunReader()
 void LOCASRunReader::Add( Int_t runID )
 {
 
+  // Create the full system path of the LOCASRun file.
   stringstream myStream;
   myStream << runID;
 
@@ -172,6 +214,7 @@ void LOCASRunReader::Add( Int_t runID )
   
   string filename = ( locasRunDir + runIDStr + fExt );
 
+  // Add the file to the reader.
   Long64_t total = fNLOCASRuns;
 
   fLOCASRunT->Add( filename.c_str() );
@@ -188,12 +231,20 @@ void LOCASRunReader::Add( Int_t runID )
 void LOCASRunReader::Add( const char* filename )
 {
 
+  // Get the current total number of LOCASRuns
+  // stored on the reader.
   Long64_t total = fNLOCASRuns;
 
+  // Add the file.
   fLOCASRunT->Add( filename );
+
+  // Get the entry and add the run ID on the newly
+  // added file to add to the list.
   fLOCASRunT->GetEntry( total );
   fListOfRunIDs.push_back( fLOCASRun->GetRunID() );
 
+  // Recalculate the total number of LOCASRuns held
+  // by the reader (this should just increment by one in this case).
   fNLOCASRuns = fLOCASRunT->GetEntries();
 
 }
@@ -204,15 +255,21 @@ void LOCASRunReader::Add( const char* filename )
 Bool_t LOCASRunReader::CheckForLOCASRun( Int_t runID )
 {
 
+  // Assume the run doesn't exist to begin with.
   Bool_t existRunID = false;
 
+  // Check through each of the run IDs and see if there are
+  // any matching files.
   for ( Int_t iID = 0; iID < (Int_t)fListOfRunIDs.size(); iID++ ){
     
+    // If there is a matching file, set the boolean
+    // variable 'existRunID' to true and break out of the loop.
     if ( fListOfRunIDs[ iID ] == runID ){ existRunID = true; break; }
     else { continue; }
 
   }
 
+  // Return the boolean value.
   return existRunID;
 
 }
@@ -223,20 +280,26 @@ Bool_t LOCASRunReader::CheckForLOCASRun( Int_t runID )
 LOCASRun* LOCASRunReader::GetLOCASRun( Int_t runID )
 {
 
+  // Search through all of the LOCASRun entries currently
+  // held on the TChain 'fLOCASRunT'.
   for ( Int_t iRun = 0; iRun < fNLOCASRuns; iRun++ ){
     
     fLOCASRunT->GetEntry( iRun );
+
+    // If the run ID of one of the entries matches that
+    // of the specified run ID 'runID', then break out of 
+    // the loop. Otherwise continue to check.
     if ( fLOCASRun->GetRunID() == runID ){ break; }
     else { continue; }
 
   }
 
-  std::map< Int_t, LOCASPMT >::iterator iPMT = fLOCASRun->GetLOCASPMTIterBegin();
+  // Set the current PMT pointer to that of the first LOCASPMT object
+  // on the current (pointed to) LOCASRun object.
+  map< Int_t, LOCASPMT >::iterator iPMT = fLOCASRun->GetLOCASPMTIterBegin();
   fCurrentPMT = &( iPMT->second );
 
+  // Return the pointer to the LOCASRun object.
   return fLOCASRun;
+
 }
-
-
-
-
--- src/LOCASRunReader.hh
+++ src/LOCASRunReader.hh
@@ -8,25 +8,27 @@
 ///          
 /// AUTHOR: Rob Stainforth [RPFS] <rpfs@liv.ac.uk>
 ///
-/// REVISION HISTORY:\n
-///     02/2014 : RPFS - First Revision, new file. \n
+/// REVISION HISTORY:
+///     04/2015 : RPFS - First Revision, new file.
 ///
 /// DETAIL: This class is used for easily reading in the LOCASRun
 ///         files. This is similar to the SOCReader class used
 ///         in RAT for RAT::DS::SOC objects but for LOCAS::LOCASRun
-///         objects
+///         objects.
 ///
 ////////////////////////////////////////////////////////////////////
 
 #ifndef _LOCASRUNREADER_
 #define _LOCASRUNREADER_
 
-#include <TObject.h>
-#include <TChain.h>
+#include "LOCASRun.hh"
+
+#include "TObject.h"
+#include "TChain.h"
 
 #include <vector>
 
-#include "LOCASRun.hh"
+using namespace std;
 
 namespace LOCAS{
 
@@ -34,19 +36,26 @@ namespace LOCAS{
   {
   public:
 
+    // The constructors and destructor for the LOCASRunReader object.
     LOCASRunReader();
-    LOCASRunReader( std::vector< Int_t >& runIDs );
+    LOCASRunReader( vector< Int_t >& runIDs );
     LOCASRunReader( Int_t runID );
     LOCASRunReader( const char* filename );
-    virtual ~LOCASRunReader();
+    ~LOCASRunReader();
 
     /////////////////////////////////
     ////////     METHODS     ////////
     /////////////////////////////////
 
+    // Add a LOCASRun file by runID.
     void Add( Int_t runID );
+
+    // Add a LOCASRun file as specified by a list of
+    // run IDs in a 'fit-file'
     void Add( const char* filename );
 
+    // Check to see if a LOCASRun object given by the 'runID'
+    // is currently accessible. i.e. has it been 'added' yet.
     Bool_t CheckForLOCASRun( Int_t runID );
 
     /////////////////////////////////
@@ -60,35 +69,38 @@ namespace LOCAS{
     LOCASRun* GetLOCASRun( Int_t runID );
 
     // Returns the 'runEntry'-th entry, e.g. for 4 files on the LOCASRunReader
-    // object, GetRunEntry( 1 ) would return the SECOND run file ( 0 being the first )
-    LOCASRun* GetRunEntry( Int_t runEntry ){ fLOCASRunT->GetEntry( runEntry ); return fLOCASRun; } 
+    // object, GetRunEntry( 1 ) would return the SECOND run file 
+    // ( 0 being the first )
+    LOCASRun* GetRunEntry( Int_t runEntry ){ 
+      fLOCASRunT->GetEntry( runEntry );
+      return fLOCASRun;
+    } 
 
     // Returns the number of LOCASRuns on the reader object
     Long64_t GetNLOCASRuns(){ return fNLOCASRuns; }
 
     // Returns a vector of runIDs
-    std::vector< Int_t > GetListOfRunIDs(){ return fListOfRunIDs; }
+    vector< Int_t > GetListOfRunIDs(){ return fListOfRunIDs; }
 
     /////////////////////////////////
     ////////     SETTERS     ////////
     /////////////////////////////////
 
+    // Set the current LOCASRun by run ID.
     void SetCurrentLOCASRun( Int_t runID ){ fLOCASRun = GetLOCASRun( runID ); }
-
-    
 
   protected:
 
-    TChain* fLOCASRunT;                   //! TChain of the ROOT files added to the LOCASRun reader
-    LOCASRun* fLOCASRun;                  //! Pointer to the current LOCASRun TTree
+    TChain* fLOCASRunT;                   // TChain of the ROOT files added to the LOCASRun reader.
+    LOCASRun* fLOCASRun;                  // Pointer to the current LOCASRun object.
     
-    Long64_t fNext;                       //! Index of the next LOCASRun TTree in the TChain
-    Long64_t fNLOCASRuns;                 //! Total number of LOCASRun TTrees in the TChain
+    Long64_t fNext;                       // Index of the next LOCASRun TTree in the TChain.
+    Long64_t fNLOCASRuns;                 // Total number of LOCASRun TTrees in the TChain.
 
-    std::vector< Int_t > fListOfRunIDs;   //! Vector of RunIDs whose corresponding ROOT fies
-                                          // have been loaded onto the TChain
+    vector< Int_t > fListOfRunIDs;        // Vector of run IDs whose corresponding ROOT fies
+                                          // have been loaded onto the TChain.
 
-    LOCASPMT* fCurrentPMT;                //! Pointer to the current LOCASPMT object
+    LOCASPMT* fCurrentPMT;                // Pointer to the current LOCASPMT object.
 
     ClassDef( LOCASRunReader, 0 ) 
     
