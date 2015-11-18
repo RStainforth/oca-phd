ce9d38f49d8a222b964c6f1189f0b23e079f8e80
RStainforth <rob@MacBook-Pro.local>
2015-11-18 10:48:44 +0000

    Added oca2fit and fitfiles

---
+++ scripts/oca2fit/oca2fit_labppote0p3perylene.sh
@@ -0,0 +1,103 @@
+source ${OCA_SNOPLUS_ROOT}/env_oca_snoplus.env
+
+# Oct15, Labppote0p3perylene, 337 nm [MC]
+oca2fit -f oct15_labppote0p3perylene_337.ocadb -b nominal
+oca2fit -f oct15_labppote0p3perylene_337.ocadb -b laserball_r_scale
+oca2fit -f oct15_labppote0p3perylene_337.ocadb -b laserball_r_shift
+oca2fit -f oct15_labppote0p3perylene_337.ocadb -b laserball_minus_x_shift
+oca2fit -f oct15_labppote0p3perylene_337.ocadb -b laserball_plus_x_shift
+oca2fit -f oct15_labppote0p3perylene_337.ocadb -b laserball_minus_y_shift
+oca2fit -f oct15_labppote0p3perylene_337.ocadb -b laserball_plus_y_shift
+oca2fit -f oct15_labppote0p3perylene_337.ocadb -b laserball_minus_z_shift
+oca2fit -f oct15_labppote0p3perylene_337.ocadb -b laserball_plus_z_shift
+oca2fit -f oct15_labppote0p3perylene_337.ocadb -b lambda_minus_shift
+oca2fit -f oct15_labppote0p3perylene_337.ocadb -b lamda_plus_shift
+oca2fit -f oct15_labppote0p3perylene_337.ocadb -b laserball_distribution2
+oca2fit -f oct15_labppote0p3perylene_337.ocadb -b laserball_distribution_flat
+oca2fit -f oct15_labppote0p3perylene_337.ocadb -b chi_square_lim_16
+oca2fit -f oct15_labppote0p3perylene_337.ocadb -b chi_square_lim_9
+
+# Oct15, Labppote0p3perylene, 369 nm [MC]
+oca2fit -f oct15_labppote0p3perylene_369.ocadb -b nominal
+oca2fit -f oct15_labppote0p3perylene_369.ocadb -b laserball_r_scale
+oca2fit -f oct15_labppote0p3perylene_369.ocadb -b laserball_r_shift
+oca2fit -f oct15_labppote0p3perylene_369.ocadb -b laserball_minus_x_shift
+oca2fit -f oct15_labppote0p3perylene_369.ocadb -b laserball_plus_x_shift
+oca2fit -f oct15_labppote0p3perylene_369.ocadb -b laserball_minus_y_shift
+oca2fit -f oct15_labppote0p3perylene_369.ocadb -b laserball_plus_y_shift
+oca2fit -f oct15_labppote0p3perylene_369.ocadb -b laserball_minus_z_shift
+oca2fit -f oct15_labppote0p3perylene_369.ocadb -b laserball_plus_z_shift
+oca2fit -f oct15_labppote0p3perylene_369.ocadb -b lambda_minus_shift
+oca2fit -f oct15_labppote0p3perylene_369.ocadb -b lamda_plus_shift
+oca2fit -f oct15_labppote0p3perylene_369.ocadb -b laserball_distribution2
+oca2fit -f oct15_labppote0p3perylene_369.ocadb -b laserball_distribution_flat
+oca2fit -f oct15_labppote0p3perylene_369.ocadb -b chi_square_lim_16
+oca2fit -f oct15_labppote0p3perylene_369.ocadb -b chi_square_lim_9
+
+# Oct15, Labppote0p3perylene, 385 nm [MC]
+oca2fit -f oct15_labppote0p3perylene_385.ocadb -b nominal
+oca2fit -f oct15_labppote0p3perylene_385.ocadb -b laserball_r_scale
+oca2fit -f oct15_labppote0p3perylene_385.ocadb -b laserball_r_shift
+oca2fit -f oct15_labppote0p3perylene_385.ocadb -b laserball_minus_x_shift
+oca2fit -f oct15_labppote0p3perylene_385.ocadb -b laserball_plus_x_shift
+oca2fit -f oct15_labppote0p3perylene_385.ocadb -b laserball_minus_y_shift
+oca2fit -f oct15_labppote0p3perylene_385.ocadb -b laserball_plus_y_shift
+oca2fit -f oct15_labppote0p3perylene_385.ocadb -b laserball_minus_z_shift
+oca2fit -f oct15_labppote0p3perylene_385.ocadb -b laserball_plus_z_shift
+oca2fit -f oct15_labppote0p3perylene_385.ocadb -b lambda_minus_shift
+oca2fit -f oct15_labppote0p3perylene_385.ocadb -b lamda_plus_shift
+oca2fit -f oct15_labppote0p3perylene_385.ocadb -b laserball_distribution2
+oca2fit -f oct15_labppote0p3perylene_385.ocadb -b laserball_distribution_flat
+oca2fit -f oct15_labppote0p3perylene_385.ocadb -b chi_square_lim_16
+oca2fit -f oct15_labppote0p3perylene_385.ocadb -b chi_square_lim_9
+
+# Oct15, Labppote0p3perylene, 420 nm [MC]
+oca2fit -f oct15_labppote0p3perylene_420.ocadb -b nominal
+oca2fit -f oct15_labppote0p3perylene_420.ocadb -b laserball_r_scale
+oca2fit -f oct15_labppote0p3perylene_420.ocadb -b laserball_r_shift
+oca2fit -f oct15_labppote0p3perylene_420.ocadb -b laserball_minus_x_shift
+oca2fit -f oct15_labppote0p3perylene_420.ocadb -b laserball_plus_x_shift
+oca2fit -f oct15_labppote0p3perylene_420.ocadb -b laserball_minus_y_shift
+oca2fit -f oct15_labppote0p3perylene_420.ocadb -b laserball_plus_y_shift
+oca2fit -f oct15_labppote0p3perylene_420.ocadb -b laserball_minus_z_shift
+oca2fit -f oct15_labppote0p3perylene_420.ocadb -b laserball_plus_z_shift
+oca2fit -f oct15_labppote0p3perylene_420.ocadb -b lambda_minus_shift
+oca2fit -f oct15_labppote0p3perylene_420.ocadb -b lamda_plus_shift
+oca2fit -f oct15_labppote0p3perylene_420.ocadb -b laserball_distribution2
+oca2fit -f oct15_labppote0p3perylene_420.ocadb -b laserball_distribution_flat
+oca2fit -f oct15_labppote0p3perylene_420.ocadb -b chi_square_lim_16
+oca2fit -f oct15_labppote0p3perylene_420.ocadb -b chi_square_lim_9
+
+# Oct15, Labppote0p3perylene, 490 nm [MC]
+oca2fit -f oct15_labppote0p3perylene_490.ocadb -b nominal
+oca2fit -f oct15_labppote0p3perylene_490.ocadb -b laserball_r_scale
+oca2fit -f oct15_labppote0p3perylene_490.ocadb -b laserball_r_shift
+oca2fit -f oct15_labppote0p3perylene_490.ocadb -b laserball_minus_x_shift
+oca2fit -f oct15_labppote0p3perylene_490.ocadb -b laserball_plus_x_shift
+oca2fit -f oct15_labppote0p3perylene_490.ocadb -b laserball_minus_y_shift
+oca2fit -f oct15_labppote0p3perylene_490.ocadb -b laserball_plus_y_shift
+oca2fit -f oct15_labppote0p3perylene_490.ocadb -b laserball_minus_z_shift
+oca2fit -f oct15_labppote0p3perylene_490.ocadb -b laserball_plus_z_shift
+oca2fit -f oct15_labppote0p3perylene_490.ocadb -b lambda_minus_shift
+oca2fit -f oct15_labppote0p3perylene_490.ocadb -b lamda_plus_shift
+oca2fit -f oct15_labppote0p3perylene_490.ocadb -b laserball_distribution2
+oca2fit -f oct15_labppote0p3perylene_490.ocadb -b laserball_distribution_flat
+oca2fit -f oct15_labppote0p3perylene_490.ocadb -b chi_square_lim_16
+oca2fit -f oct15_labppote0p3perylene_490.ocadb -b chi_square_lim_9
+
+# Oct15, Labppote0p3perylene, 505 nm [MC]
+oca2fit -f oct15_labppote0p3perylene_505.ocadb -b nominal
+oca2fit -f oct15_labppote0p3perylene_505.ocadb -b laserball_r_scale
+oca2fit -f oct15_labppote0p3perylene_505.ocadb -b laserball_r_shift
+oca2fit -f oct15_labppote0p3perylene_505.ocadb -b laserball_minus_x_shift
+oca2fit -f oct15_labppote0p3perylene_505.ocadb -b laserball_plus_x_shift
+oca2fit -f oct15_labppote0p3perylene_505.ocadb -b laserball_minus_y_shift
+oca2fit -f oct15_labppote0p3perylene_505.ocadb -b laserball_plus_y_shift
+oca2fit -f oct15_labppote0p3perylene_505.ocadb -b laserball_minus_z_shift
+oca2fit -f oct15_labppote0p3perylene_505.ocadb -b laserball_plus_z_shift
+oca2fit -f oct15_labppote0p3perylene_505.ocadb -b lambda_minus_shift
+oca2fit -f oct15_labppote0p3perylene_505.ocadb -b lamda_plus_shift
+oca2fit -f oct15_labppote0p3perylene_505.ocadb -b laserball_distribution2
+oca2fit -f oct15_labppote0p3perylene_505.ocadb -b laserball_distribution_flat
+oca2fit -f oct15_labppote0p3perylene_505.ocadb -b chi_square_lim_16
+oca2fit -f oct15_labppote0p3perylene_505.ocadb -b chi_square_lim_9
---
+++ scripts/oca2fit/oca2fit_water.sh
@@ -0,0 +1,86 @@
+source ${OCA_SNOPLUS_ROOT}/env_oca_snoplus.env
+
+# Oct15, Water, 337 nm [MC]
+oca2fit -f oct15_water_337.ocadb -b nominal
+oca2fit -f oct15_water_337.ocadb -b laserball_r_scale
+oca2fit -f oct15_water_337.ocadb -b laserball_r_shift
+oca2fit -f oct15_water_337.ocadb -b laserball_minus_x_shift
+oca2fit -f oct15_water_337.ocadb -b laserball_plus_x_shift
+oca2fit -f oct15_water_337.ocadb -b laserball_minus_y_shift
+oca2fit -f oct15_water_337.ocadb -b laserball_plus_y_shift
+oca2fit -f oct15_water_337.ocadb -b laserball_minus_z_shift
+oca2fit -f oct15_water_337.ocadb -b laserball_plus_z_shift
+oca2fit -f oct15_water_337.ocadb -b lambda_minus_shift
+oca2fit -f oct15_water_337.ocadb -b lamda_plus_shift
+oca2fit -f oct15_water_337.ocadb -b laserball_distribution2
+oca2fit -f oct15_water_337.ocadb -b laserball_distribution_flat
+oca2fit -f oct15_water_337.ocadb -b chi_square_lim_16
+oca2fit -f oct15_water_337.ocadb -b chi_square_lim_9
+
+# Oct15, Water, 369 nm [MC]
+oca2fit -f oct15_water_369.ocadb -b nominal
+oca2fit -f oct15_water_369.ocadb -b laserball_r_scale
+oca2fit -f oct15_water_369.ocadb -b laserball_r_shift
+oca2fit -f oct15_water_369.ocadb -b laserball_minus_x_shift
+oca2fit -f oct15_water_369.ocadb -b laserball_plus_x_shift
+oca2fit -f oct15_water_369.ocadb -b laserball_minus_y_shift
+oca2fit -f oct15_water_369.ocadb -b laserball_plus_y_shift
+oca2fit -f oct15_water_369.ocadb -b laserball_minus_z_shift
+oca2fit -f oct15_water_369.ocadb -b laserball_plus_z_shift
+oca2fit -f oct15_water_369.ocadb -b lambda_minus_shift
+oca2fit -f oct15_water_369.ocadb -b lamda_plus_shift
+oca2fit -f oct15_water_369.ocadb -b laserball_distribution2
+oca2fit -f oct15_water_369.ocadb -b laserball_distribution_flat
+oca2fit -f oct15_water_369.ocadb -b chi_square_lim_16
+oca2fit -f oct15_water_369.ocadb -b chi_square_lim_9
+
+# Oct15, Water, 385 nm [MC]
+oca2fit -f oct15_water_385.ocadb -b nominal
+oca2fit -f oct15_water_385.ocadb -b laserball_r_scale
+oca2fit -f oct15_water_385.ocadb -b laserball_r_shift
+oca2fit -f oct15_water_385.ocadb -b laserball_minus_x_shift
+oca2fit -f oct15_water_385.ocadb -b laserball_plus_x_shift
+oca2fit -f oct15_water_385.ocadb -b laserball_minus_y_shift
+oca2fit -f oct15_water_385.ocadb -b laserball_plus_y_shift
+oca2fit -f oct15_water_385.ocadb -b laserball_minus_z_shift
+oca2fit -f oct15_water_385.ocadb -b laserball_plus_z_shift
+oca2fit -f oct15_water_385.ocadb -b lambda_minus_shift
+oca2fit -f oct15_water_385.ocadb -b lamda_plus_shift
+oca2fit -f oct15_water_385.ocadb -b laserball_distribution2
+oca2fit -f oct15_water_385.ocadb -b laserball_distribution_flat
+oca2fit -f oct15_water_385.ocadb -b chi_square_lim_16
+oca2fit -f oct15_water_385.ocadb -b chi_square_lim_9
+
+# Oct15, Water, 420 nm [MC]
+oca2fit -f oct15_water_420.ocadb -b nominal
+oca2fit -f oct15_water_420.ocadb -b laserball_r_scale
+oca2fit -f oct15_water_420.ocadb -b laserball_r_shift
+oca2fit -f oct15_water_420.ocadb -b laserball_minus_x_shift
+oca2fit -f oct15_water_420.ocadb -b laserball_plus_x_shift
+oca2fit -f oct15_water_420.ocadb -b laserball_minus_y_shift
+oca2fit -f oct15_water_420.ocadb -b laserball_plus_y_shift
+oca2fit -f oct15_water_420.ocadb -b laserball_minus_z_shift
+oca2fit -f oct15_water_420.ocadb -b laserball_plus_z_shift
+oca2fit -f oct15_water_420.ocadb -b lambda_minus_shift
+oca2fit -f oct15_water_420.ocadb -b lamda_plus_shift
+oca2fit -f oct15_water_420.ocadb -b laserball_distribution2
+oca2fit -f oct15_water_420.ocadb -b laserball_distribution_flat
+oca2fit -f oct15_water_420.ocadb -b chi_square_lim_16
+oca2fit -f oct15_water_420.ocadb -b chi_square_lim_9
+
+# Oct15, Water, 505 nm [MC]
+oca2fit -f oct15_water_505.ocadb -b nominal
+oca2fit -f oct15_water_505.ocadb -b laserball_r_scale
+oca2fit -f oct15_water_505.ocadb -b laserball_r_shift
+oca2fit -f oct15_water_505.ocadb -b laserball_minus_x_shift
+oca2fit -f oct15_water_505.ocadb -b laserball_plus_x_shift
+oca2fit -f oct15_water_505.ocadb -b laserball_minus_y_shift
+oca2fit -f oct15_water_505.ocadb -b laserball_plus_y_shift
+oca2fit -f oct15_water_505.ocadb -b laserball_minus_z_shift
+oca2fit -f oct15_water_505.ocadb -b laserball_plus_z_shift
+oca2fit -f oct15_water_505.ocadb -b lambda_minus_shift
+oca2fit -f oct15_water_505.ocadb -b lamda_plus_shift
+oca2fit -f oct15_water_505.ocadb -b laserball_distribution2
+oca2fit -f oct15_water_505.ocadb -b laserball_distribution_flat
+oca2fit -f oct15_water_505.ocadb -b chi_square_lim_16
+oca2fit -f oct15_water_505.ocadb -b chi_square_lim_9
